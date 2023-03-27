7cf8bac23f8403a53d46c11ec512cdfb9a6dedd1
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-03-27 23:20:30 +0530

    Displaying Posts

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,10 @@ export default function Home({ posts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='grid- grid-cols-1 lg:grid-cols-12 gap-12'>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
 
         <div className='col-span-1 lg:col-span-8'>
-          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
         </div>
 
         <div className='col-span-1 lg:col-span-4'>
